4e2ba1139441c8a263b43dbe5a33d2a89e8243df
crypt0box <pray.happy76@gmail.com>
2021-10-09 16:01:37 +0900

    feat: implement signin/out feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Amplify from 'aws-amplify'
+import config from '../src/aws-exports'
+Amplify.configure({
+  ...config,
+  ssr: true
+})
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <nav>
+        <Link href="/">
+          <span>Home</span>
+        </Link>
+        <Link href="/profile">
+          <span>Profile</span>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
-export default MyApp
